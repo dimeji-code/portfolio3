6bba2c603ab10940155781b0eed1e3a7fea13d10
dJ <34945097+dimeji-code@users.noreply.github.com>
2025-05-28 13:53:51 -0400

    Update Experience.tsx

--- components/Experience.tsx
+++ components/Experience.tsx
@@ -4,10 +4,10 @@ import WorkBox from './WorkBox';
 type Props = {}
 
 const Experience = (props: Props) => {
-  const ukemeyTools:string[] = ['CSS','Git','Typescript','React JS','React Native','MongoDB','AWS','PuppeteerJS','Google Tag Manager']
-  const freelanceTools:string[] = ['JavaScript','Typescript','React JS','React Native','MongoDB','Firebase','Tailwind CSS',"HTML"]
+  const ukemeyTools:string[] = ['Typescript','ReactJS','React Native','MongoDB','AWS','Google Tag Manager']
+  const freelanceTools:string[] = ['JavaScript','React','React Native',"HTML"]
   const ukemeyJob = {company:"Ukemey Inc.", role:"Fullstack developer", loc:"Ottawa (Remote)",dur:"Sept 2021 - Mar 2022", desc:"Developed mobile and web based applications for clients of the company. Use of automation tools for webscraping data."}
-    const freelance = {company:"Decor & Vibes", role:"Full stack developer", loc:"Ottawa (Remote)",dur:"Nov 2022 - Sept 2024", desc:"Using React and React native to turn wireframe designs into functional and fully responsive web applications for clients."}
+    const freelance = {company:"Decor & Vibes", role:"Full stack developer & Consultant", loc:"Ottawa (Remote)",dur:"Nov 2022 - Sept 2024", desc:"Using React and React native to turn wireframe designs into functional and fully responsive web applications for clients."}
 return(
     <div className='flex flex-col items-center  w-full h-[100vh] py-4 mx-auto justify-center '>
       {/* <div className='flex flex-1 flex-col w-full md:w-5/6 h-3/5 rounded-2xl  options overflow-hidden'> */}
