b3e1d38d4f5d90fbf47ab9241c00d5f46ad1298e
dJ <34945097+dimeji-code@users.noreply.github.com>
2026-04-12 12:23:07 -0400

    Update Wordpressify.tsx

--- components/Wordpressify.tsx
+++ components/Wordpressify.tsx
@@ -12,7 +12,7 @@ const Wordpressify = (props: Props) => {
         
     <div className="w-full h-[35vh]  p-8 mx-auto max-w-6xl   flex flex-row items-center hover:cursor-pointer " onClick={()=> window.open('https://dimeji-code.github.io/Food-Quality-Assurance/', "_blank")} >
         <div className="w-1/2 h-full flex-1/3 flex justify-center">
-         <img src="/svgs/wordpress.svg"  alt='wordpress' className='w-auto h-auto hover:cursor-pointer rotate hover:scale-y-2'  />
+         <img src="/svgs/articulate360.png"  alt='articulate' className='w-auto h-auto hover:cursor-pointer rotate hover:scale-y-2'  />
 
         </div>
         {/* <h2>Hello wordpress</h2> */}
