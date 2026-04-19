f889858403d64135dfcf34aea9dbd5bb9ef6ec9a
dJ <34945097+dimeji-code@users.noreply.github.com>
2026-04-19 07:02:27 -0400

    Update Wordpressify.tsx

--- components/Wordpressify.tsx
+++ components/Wordpressify.tsx
@@ -12,12 +12,12 @@ const Wordpressify = (props: Props) => {
         
     <div className="w-full h-[35vh]  p-8 mx-auto max-w-6xl   flex flex-row items-center hover:cursor-pointer " onClick={()=> window.open('https://dimeji-code.github.io/Food-Quality-Assurance/', "_blank")} >
         <div className="w-1/2 h-full flex-1/3 flex justify-center">
-         <img src="/articulate360.png"  alt='articulate' className='w-auto h-auto hover:cursor-pointer rotate hover:scale-y-2'  />
+         <img src="/storyline.png"  alt='articulate' className='w-auto h-auto hover:cursor-pointer rotate hover:scale-y-2'  />
 
         </div>
         {/* <h2>Hello wordpress</h2> */}
         <div className="w-1/2 flex-3/4 h-full flex flex-col justify-center text-left items-left">
-            <h5 className='text-[#263241] text-xl md:text-3xl hover:cursor-pointer'  >Click here to view my course made with <strong>ARTICULATE 360</strong>.</h5>
+            <h5 className='text-[#263241] text-xl md:text-3xl hover:cursor-pointer'  >Click here to view my course made with Articulate 360 <strong>[ STORYLINE 360 ]</strong>.</h5>
 
         </div>
 
