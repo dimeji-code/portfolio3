0f71bc67b95489520c2f3a57032d09e3831ec33a
dJ <34945097+dimeji-code@users.noreply.github.com>
2026-04-23 21:44:35 -0400

    Update Rise.tsx

--- components/Rise.tsx
+++ components/Rise.tsx
@@ -11,8 +11,8 @@ const Rise = (props: Props) => {
         </div>
         
     <div className="w-full h-[35vh]  p-8 mx-auto max-w-6xl   flex flex-row items-center hover:cursor-pointer " onClick={()=> window.open('https://dimeji-code.github.io/Learning-experience-designer/', "_blank")} >
-        <div className="w-1/2 h-full flex-1/3 flex justify-center">
-         <img src="/rise.png"  alt='articulate' className='w-auto h-auto hover:cursor-pointer rotate hover:scale-y-2'  />
+        <div className="w-1/2 h-full flex-1/3 flex justify-center items-center">
+         <img src="/rise.png"  alt='articulate' className='w-[120px] h-[120px] md:w-auto md:h-auto hover:cursor-pointer rotate hover:scale-y-2'  />
 
         </div>
         {/* <h2>Hello wordpress</h2> */}
